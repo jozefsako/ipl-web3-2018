a835ff167a850a70a1420ea59d8ac5b78e812885
Kandahar <said.imran@student.vinci.be>
2018-11-13 15:55:07 +0100

    testing 2nd commit

---
+++ src/client/react/components/paypal/paypal_container.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class PaypalContainer extends React.Component {
+  state = {};
+  render() {
+    return <h1>Testing paypal container page</h1>;
+  }
+}
+
+export default PaypalContainer;
---
+++ src/client/react/components/stripe/stripe_container.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class StripeContainer extends React.Component {
+  state = {};
+  render() {
+    return <h1>Testing of stripe container</h1>;
+  }
+}
+
+export default StripeContainer;

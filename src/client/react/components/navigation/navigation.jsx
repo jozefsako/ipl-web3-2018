af00df4daec800f850335ae6aab26c5dab91da04
Kandahar <said.imran@student.vinci.be>
2018-11-13 15:34:52 +0100

    adding stripe and paypal in navbar

--- src/client/react/components/navigation/navigation.jsx
+++ src/client/react/components/navigation/navigation.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import { Link } from 'react-router-dom';
-import SearchForm from './search_form';
-
+import Navbar from "react-bootstrap/lib/Navbar";
+import Nav from "react-bootstrap/lib/Nav";
+import { Link } from "react-router-dom";
+import SearchForm from "./search_form";
 
 const Navigation = () => {
-    return (
-        <Navbar bg="dark" variant="dark">
-        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/" >Hello</Nav.Link>
-          <Nav.Link as={Link} to="/todo" >Todo</Nav.Link>
-          <Nav.Link as={Link} to="/messages" >Messages</Nav.Link>
-          <Nav.Link as={Link} to="/login" >Login</Nav.Link>
-        </Nav>
-       <SearchForm />
-      </Navbar>
-    );
-}
-
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand as={Link} to="/">
+        Navbar
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link as={Link} to="/">
+          Hello
+        </Nav.Link>
+        <Nav.Link as={Link} to="/todo">
+          Todo
+        </Nav.Link>
+        <Nav.Link as={Link} to="/messages">
+          Messages
+        </Nav.Link>
+        <Nav.Link as={Link} to="/login">
+          Login
+        </Nav.Link>
+        <Nav.Link as={Link} to="/stripe">
+          Stripe
+        </Nav.Link>
+        <Nav.Link as={Link} to="/paypal">
+          PayPal
+        </Nav.Link>
+      </Nav>
+      <SearchForm />
+    </Navbar>
+  );
+};
 
 export default Navigation;
--- src/client/react/components/router_outlet.jsx
+++ src/client/react/components/router_outlet.jsx
@@ -8,20 +8,20 @@ import TodoAppContainer from "./todo_app/todo_app_container";
 import MessagesContainer from "./messages/messages_container";
 import MessageContainer from "./message/message_container";
 import LoginContainer from "./login/login_container";
+import StripeContainer from "./stripe/stripe_container";
+import PaypalContainer from "./paypal/paypal_container";
 
 function RouterOutlet() {
   return (
     <React.Fragment>
-      <Route
-        exact
-        path="/"
-        render={() => <HelloWorld name="bob" />}
-      />
+      <Route exact path="/" render={() => <HelloWorld name="bob" />} />
       <Route path="/hello/:name" component={HelloFromParams} />
       <Route path="/todo" component={TodoAppContainer} />
       <Route path="/messages" component={MessagesContainer} />
       <Route path="/message/:id" component={MessageContainer} />
       <Route path="/login" component={LoginContainer} />
+      <Route path="/stripe" component={StripeContainer} />
+      <Route path="/paypal" component={PaypalContainer} />
     </React.Fragment>
   );
 }

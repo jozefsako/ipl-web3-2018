458f7509fc562c96a33ae4ca691f3877ce93b675
Kandahar <said.imran@student.vinci.be>
2018-11-13 16:03:47 +0100

    updating navbar

--- src/client/react/components/router_outlet.jsx
+++ src/client/react/components/router_outlet.jsx
@@ -1,32 +1,29 @@
 import React from "react";
-import { Route, Redirect, withRouter } from "react-router-dom";
 
-import { withAuthentication } from "react/contexts/authentication";
+import { Route } from "react-router-dom";
+
 import HelloWorld from "./hello_world/hello_world";
 import HelloFromParams from "./hello_world/hello_from_params";
 import TodoAppContainer from "./todo_app/todo_app_container";
 import MessagesContainer from "./messages/messages_container";
 import MessageContainer from "./message/message_container";
 import LoginContainer from "./login/login_container";
+import StripeContainer from "./stripe/stripe_container";
+import PaypalContainer from "./paypal/paypal_container";
 
-function RouterOutlet({ isAuthenticated, location: { pathname } }) {
-  const redirectToLogin = !isAuthenticated && pathname !== "/login";
-
+function RouterOutlet() {
   return (
     <React.Fragment>
-      { redirectToLogin && <Redirect to="/login" /> }
-      { !redirectToLogin && 
-        <React.Fragment>
-          <Route exact path="/" render={() => <HelloWorld name="bob" />} />
-          <Route path="/hello/:name" component={HelloFromParams} />
-          <Route path="/todo" component={TodoAppContainer} />
-          <Route path="/messages" component={MessagesContainer} />
-          <Route path="/message/:id" component={MessageContainer} />
-          <Route path="/login" component={LoginContainer} />
-        </React.Fragment>
-      }
+      <Route exact path="/" render={() => <HelloWorld name="bob" />} />
+      <Route path="/hello/:name" component={HelloFromParams} />
+      <Route path="/todo" component={TodoAppContainer} />
+      <Route path="/messages" component={MessagesContainer} />
+      <Route path="/message/:id" component={MessageContainer} />
+      <Route path="/login" component={LoginContainer} />
+      <Route path="/stripe" component={StripeContainer} />
+      <Route path="/paypal" component={PaypalContainer} />
     </React.Fragment>
   );
 }
 
-export default withRouter(withAuthentication(RouterOutlet));
+export default RouterOutlet;

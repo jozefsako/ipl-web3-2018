5c3c3f53ce2f1ec5da24b9292ae5e57a5c9c5d7d
burim <burim.kastrati@outlook.com>
2018-11-25 23:16:48 +0000

    added module stripe

--- src/server/routes/stripe.js
+++ src/server/routes/stripe.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/*const stripe = require('stripe')(sk_test_nmpN1YJZZG07AOib8pRAsS71);
 
-routeur.post('/', (req, res) => {
+const stripe = require("stripe")("sk_test_nmpN1YJZZG07AOib8pRAsS71");
+
+router.post('/', function(req, res, next) {
     return stripe.charges
       .create({
         amount: req.body.amount, // Unit: cents
@@ -12,9 +13,6 @@ routeur.post('/', (req, res) => {
         description: 'Test payment',
       })
       .then(result => res.status(200).json(result));
-});*/   
-
-router.post('/doPayment', (req, res) => {
-  console.log(req.body);
 });
+
 module.exports = router;
